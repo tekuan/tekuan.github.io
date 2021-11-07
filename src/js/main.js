ac26fb60d8170eacf526b6f988b9efbb75e99605
tekuan <larissa_bianca@outlook.com>
2021-11-07 02:09:08 -0300

    change home page

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 function counter() {
   let seconds = 0;
   setInterval(() => {
     seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
+    document.getElementById("app").innerHTML = `<p> ${seconds} s</p>`;
   }, 1000);
 }
 
